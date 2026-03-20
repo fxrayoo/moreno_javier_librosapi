9a5cfc66d1d646acd6fb18391c561c34d1cbb165
fxrayoo <javieroasis853@gmail.com>
2026-03-20 17:42:20 +0000

    Preparación proyecto

---
+++ src/test/java/com/example/moreno_javier_librosapi/MorenoJavierLibrosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moreno_javier_librosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorenoJavierLibrosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
